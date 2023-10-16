601f5530b3b74e7127c1d8661add097982dd7c65
Ashraj Grewal <ashraj.grewal10@gmail.com>
2023-10-16 12:41:50 -0600

    add gallery

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +16,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
