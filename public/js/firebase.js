ffbb619b763d62bdc7d09e9a6ee6f9885ceca9ae
Ashraj Grewal <ashraj.grewal10@gmail.com>
2023-10-16 02:48:57 -0600

    added firebase, config html files, some css

---
+++ public/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDcn4p6Cn_zTBuv4YW3RugtTduuGxBoYqg",
+  authDomain: "defcitylife.firebaseapp.com",
+  projectId: "defcitylife",
+  storageBucket: "defcitylife.appspot.com",
+  messagingSenderId: "888056481096",
+  appId: "1:888056481096:web:dd81b993cd78479f515f7d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
