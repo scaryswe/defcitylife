0477c6235ecd6b0722c7addb764d0ff6ed099112
Ashraj Grewal <ashraj.grewal10@gmail.com>
2023-10-16 14:56:37 -0600

    Edit UI for writing blogs

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,4 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
